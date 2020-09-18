9c9d31a8e5f0b0d20be0e80068ca1c5a0cbb5e95
Yongquan96 <domesday5250@gmail.com>
2020-09-18 16:03:36 +0800

    database setup

---
+++ application/models/HomeModel.php
@@ -0,0 +1,19 @@
+<?php
+class HomeModel extends CI_Model
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	function getQuiz() {
+		$query=$this->db->query("SELECT * from quiz");
+
+		$results = array();
+		foreach ($query->result() as $result) {
+			$results[] = $result;
+		}
+		return $results;
+	}
+}
--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,11 @@
 <h1> Welcome to Project: Shark </h1>
+
+<?php
+
+foreach($quiz as $row)
+{
+	$busStop = $row->quizID;
+	echo "<th>".$busStop."</th>";
+
+}
+?>
