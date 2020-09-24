385abb83f73f9f4777a219315bd90f33c2a3240f
Yongquan96 <domesday5250@gmail.com>
2020-09-24 14:16:23 +0800

    Added fins marker

--- assets/js/ProjectSharkMap.js
+++ assets/js/ProjectSharkMap.js
@@ -1,4 +1,7 @@
 function myMap() {
+
+	var sharkSampleLocation = {lat: -23.2301768, lng: 150.8243029};
+
 	var mapProp= {
 		center:new google.maps.LatLng(-22.120754, 148.734105),
 		zoom:6,
@@ -8,5 +11,16 @@ function myMap() {
 		streetViewControl: false,
 		fullscreenControl: false,
 	};
+
 	var map = new google.maps.Map(document.getElementById("sharkMap"),mapProp);
+
+	var marker = new google.maps.Marker(
+		{
+			position: sharkSampleLocation,
+			animation:google.maps.Animation.DROP,
+			icon:'assets/img/fins.png',
+			map: map
+		})
+	;
+
 }
