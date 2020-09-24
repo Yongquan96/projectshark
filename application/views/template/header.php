94eae45bdad85c692791838e3edffb9cf0e4fd40
Yongquan96 <domesday5250@gmail.com>
2020-09-24 13:23:57 +0800

    Custom Font
    Custom Font embaded

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -6,7 +6,7 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 <!--Berlin Sans FB Demi-->
-<link href="//db.onlinewebfonts.com/c/b6539b6d3432c623d8d4f9cc2a29589e?family=Berlin+Sans+FB+Demi" rel="stylesheet" type="text/css"/>
+<!--<link href="//db.onlinewebfonts.com/c/b6539b6d3432c623d8d4f9cc2a29589e?family=Berlin+Sans+FB+Demi" rel="stylesheet" type="text/css"/>-->
 
 <link href="assets/css/ProjectSharkStyle.css" rel="stylesheet" type="text/css"/>
 
