f2101af9bd30978236ab58d2417e8e2abf913a85
Yongquan96 <domesday5250@gmail.com>
2020-09-25 17:45:18 +0800

    database setting
    update database setting

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -12,9 +12,9 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		$result['quiz']=$this->HomeModel->getQuiz();
+//		$result['quiz']=$this->HomeModel->getQuiz();
 
 		$this->load->view('template/header');
-		$this->load->view('home',$result);
+		$this->load->view('home');
 	}
 }
--- application/models/HomeModel.php
+++ application/models/HomeModel.php
@@ -8,12 +8,12 @@ class HomeModel extends CI_Model
 	}
 
 	function getQuiz() {
-		$query=$this->db->query("SELECT * from quiz");
-
-		$results = array();
-		foreach ($query->result() as $result) {
-			$results[] = $result;
-		}
-		return $results;
+//		$query=$this->db->query("SELECT * from quiz");
+//
+//		$results = array();
+//		foreach ($query->result() as $result) {
+//			$results[] = $result;
+//		}
+//		return $results;
 	}
 }
