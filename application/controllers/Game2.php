d59bb61334d9c9cc2d037de4e6fb6548933a9f56
Yongquan96 <domesday5250@gmail.com>
2020-10-26 15:30:16 +0800

    added game 2 and game menu

--- application/controllers/Game2.php
+++ application/controllers/Game2.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Game extends CI_Controller {
+class Game2 extends CI_Controller {
 
 	public function __construct()
 	{
@@ -13,6 +13,6 @@ class Game extends CI_Controller {
 		$this->load->view('template/header');
 		$this->load->view('template/subHeader');
 		$this->load->view('template/gameMenu');
-		$this->load->view('game');
+		$this->load->view('game2');
 	}
 }
---
+++ application/views/template/gameMenu.php
@@ -0,0 +1,23 @@
+<style>
+	.gameButtons{
+		font-family: "Berlin Sans FB Demi";
+		text-align: center;
+		z-index: -10;
+	}
+	.gameMenuBtn{
+		background-color: #e3f2fd;
+		color: grey;
+		border-radius: 5px;
+		padding: 5px;
+		text-decoration: none;
+	}
+	.gameMenuBtn:hover{
+		text-decoration: none;
+		color: black;
+	}
+</style>
+<div class="gameButtons">
+	<a class="gameMenuBtn" href="game">Fun Facts of sharks</a>
+	<a class="gameMenuBtn" href="game2">Guess the sharks</a>
+</div>
+
